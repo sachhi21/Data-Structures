a6308ea2feba02dc0a160c46875c260f75a89ea9
sachin rampur <sachinrampur88@gmail.com>
2025-03-25 23:49:50 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Data_Structures.Utils;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Maths.parseInt("5021"));
+    }
+}
---
+++ Utils/Maths.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures.Utils
+{
+    public class Maths
+    {
+       
+
+        public static int parseInt(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                throw new ArgumentException("Input string cannot be null or empty.");
+
+            int sum = 0;
+            int multiplier = 1;
+
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                char c = s[i];
+
+                if (c < '0' || c > '9')
+                    throw new FormatException("Input string contains non-numeric characters.");
+
+                sum += (c - '0') * multiplier;
+                multiplier *= 10;
+                                  
+            }
+
+            return sum;
+        }
+    }
+
+}
